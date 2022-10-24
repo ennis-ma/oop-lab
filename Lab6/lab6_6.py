dcd7e0a6ac902890d5d7d0a14850b74615b2383a
ennis-ma <ennis@hustle.hk>
2022-10-24 16:15:00 -0600

    lab6_6

--- Lab6/lab6_6.py
+++ Lab6/lab6_6.py
@@ -1,5 +1,6 @@
-given_tuple = (1, 2, 3, 4, 5, 6, 7, 8)
-avg = 0
-for v in given_tuple:
-    avg += v / len(given_tuple)
-print(avg)
+given_tuple = ((1, 2, 3), (4, 5, 6), (7, 8))
+for t in given_tuple:
+    avg = 0
+    for v in t:
+        avg += v / len(t)
+    print(avg)
